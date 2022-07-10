5cbb69ea20cdd3a423b41a8cd3cf31ffebec1bc5
Keelhauled <10797970+Keelhauled@users.noreply.github.com>
2022-07-10 22:24:29 +0300

    Organize hotkeys

--- src/tubular/MainWindow.cs
+++ src/tubular/MainWindow.cs
@@ -26,37 +26,16 @@ namespace Tubular
             var filterText = new TextField(){ Width = Dim.Fill(), Height = 1, ColorScheme = Colors.Dialog };
             var videoList = new ListView(filteredVideos.Select(x => x.title).ToList()){ Y = Pos.Bottom(filterText), Width = Dim.Fill(), Height = Dim.Fill() };
 
-            videoList.OpenSelectedItem += x =>
-            {
-                var entry = filteredVideos[x.Item].entry;
-                var val = MessageBox.Query("", $"{entry.Author.Name}\n{entry.Title}\n{entry.Published}", "Play", "Link", "Channel", "Cancel");
-                switch(val)
-                {
-                    case 0:
-                        Utils.StartRedirectedProcess("mpv", entry.Link.Href);
-                        MessageBox.Query("", $"Playing video:\n{entry.Title}", "OK");
-                        break;
-                    
-                    case 1:
-                        Process.Start(new ProcessStartInfo(entry.Link.Href){ UseShellExecute = true });
-                        break;
-                    
-                    case 2:
-                        Process.Start(new ProcessStartInfo(entry.Author.Uri){ UseShellExecute = true });
-                        break;
-                }
-            };
-
             videoList.RowRender += args =>
             {
                 if(args.Row == videoList.SelectedItem)
                     return;
                 
                 var author = filteredVideos[args.Row].entry.Author.Name;
-
                 if(!authorColors.TryGetValue(author, out var color))
                 {
-                    var colors = Enum.GetValues<Color>().Except(new[]{Color.Black, Color.DarkGray}).ToArray();
+                    var blacklist = new[]{Color.Black, Color.DarkGray, Color.Magenta, Color.Brown};
+                    var colors = Enum.GetValues<Color>().Except(blacklist).ToArray();
                     color = colors[new Random().Next(colors.Length)];
                     authorColors.Add(author, color);
                 }
@@ -75,9 +54,9 @@ namespace Tubular
 
             var statusBar = new StatusBar(new []
             {
-                new StatusItem(Key.CtrlMask | Key.q, "~Ctrl+Q~ Quit", () => {}),
-                new StatusItem(Key.CtrlMask | Key.f, "~Ctrl+F~ Filter", () => filterText.SetFocus()),
-                new StatusItem(Key.p, "~P~ Play", () =>
+                new StatusItem(Key.CtrlMask | Key.Q, "~Ctrl+Q~ Quit", () => Application.RequestStop()),
+                new StatusItem(Key.CtrlMask | Key.F, "~Ctrl+F~ Filter", () => filterText.SetFocus()),
+                new StatusItem(Key.Enter, "~Enter~ Play", () =>
                 {
                     var entry = filteredVideos[videoList.SelectedItem].entry;
                     Utils.StartRedirectedProcess("mpv", entry.Link.Href);
@@ -85,21 +64,27 @@ namespace Tubular
                 }),
                 new StatusItem(Key.l, "~L~ Latest", () =>
                 {
-                    if(showingLatest)
-                    {
-                        filteredVideos = allVideos.ToList();
-                        videoList.SetSource(filteredVideos.Select(y => y.title).ToList());
-                    }
-                    else
-                    {
-                        filteredVideos = allVideos.GroupBy(x => x.entry.Author.Name).Select(x => x.First()).ToList();
-                        videoList.SetSource(filteredVideos.Select(y => y.title).ToList());
-                    }
-
+                    filteredVideos = (showingLatest ? allVideos : allVideos.GroupBy(x => x.entry.Author.Name).Select(x => x.First())).ToList();
+                    videoList.SetSource(filteredVideos.Select(y => y.title).ToList());
                     showingLatest = !showingLatest;
+                }),
+                new StatusItem(Key.i, "~I~ Info", () =>
+                {
+                    var entry = filteredVideos[videoList.SelectedItem].entry;
+                    var val = MessageBox.Query("", $"{entry.Author.Name}\n{entry.Title}\n{entry.Published}", "Link", "Channel", "Cancel");
+                    switch(val)
+                    {
+                        case 0:
+                            Process.Start(new ProcessStartInfo(entry.Link.Href){ UseShellExecute = true });
+                            break;
+                    
+                        case 1:
+                            Process.Start(new ProcessStartInfo(entry.Author.Uri){ UseShellExecute = true });
+                            break;
+                    }
                 })
             });
-            
+
             Add(filterText, videoList, statusBar);
         }
     }
